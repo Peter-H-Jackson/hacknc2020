5c0e13c84aadb62a92b6e5f8b7cec10ada8ebcea
Peter Jackson <peterhj@live.unc.edu>
2020-10-17 18:44:25 -0400

    Oooooommmyyygodddddd this is soooooo sexyyyyy

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import Expenses from './Expenses.js';
 
-function dosomething() {
-  var name, amount, type, date;
+function doSomething() {
 
-  name = document.getElementById("expnameinput").value;
-  amount = document.getElementById("expamountinput").value;
-  type = document.getElementById("exptypeinput").value;
-  date = document.getElementById("expdateinput").value;
+  var expense = new Expense(
+  document.getElementById("expnameinput").value,
+  document.getElementById("expamountinput").value,
+  document.getElementById("exptypeinput").value,
+  document.getElementById("expdateinput").value
+  );
 
-  var expense = new Expense(name, date, type, amount);
-
-  document.getElementById("out_name").innerHTML = expense.getName();
-  document.getElementById("out_amount").innerHTML = expense.getAmount();
-  document.getElementById("out_type").innerHTML = expense.getType();
-  document.getElementById("out_date").innerHTML = expense.getDate();
+  document.getElementById("out_name").innerHTML = "fuck you";
 
 }
