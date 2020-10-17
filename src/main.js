da209ad6a5da17446066cc96b1d2e3ac80f93670
Peter Jackson <peterhj@live.unc.edu>
2020-10-17 18:56:23 -0400

    Oooooommmyyygodddddd this is soooooo sexyyyyy

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import Expenses from './Expenses.js';
 
-function doSomething() {
+function myFunction() {
 
+/*
   var expense = new Expense(
   document.getElementById("expnameinput").value,
   document.getElementById("expamountinput").value,
   document.getElementById("exptypeinput").value,
   document.getElementById("expdateinput").value
   );
+  */
 
-  document.getElementById("out_name").innerHTML = "fuck you";
+  document.getElementById("output").innerHTML = "fuck you";
 
 }
