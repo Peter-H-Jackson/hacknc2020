431d75bd90266c420755bff383d030c5175ae6eb
Peter Jackson <peterhj@live.unc.edu>
2020-10-17 13:46:50 -0400

    commit

--- src/main.js
+++ src/main.js
@@ -1,35 +1,32 @@
-// Global Variables
-var expense_name, expense_amount, expense_date
-expense_amount = document.getElementById("expamountinput").value;
-expense_name = document.getElementById("expnameinput").value;
-expense_date = document.getElementById("expdateinput").value;
-
 function submit() {
+  var expense_name, expense_amount, expense_date
+  expense_name = document.getElementById("expnameinput").value;
+  expense_date = document.getElementById("expdateinput").value;
+  expense_amount = document.getElementById("expamountinput").value;
 
   if (expense_amount == null || expense_amount == NaN || parseFloat(expense_amount) < 0) {
     window.alert("Invalid amount input!");
   }
 
   if (document.getElementById("expensetype").value == "onetimeexpense") {
-    one_time_expense();
+    one_time_expense(expense_name, expense_amount, expense_date);
   } else if (document.getElementById("expensetype").value == "subscription") {
-    subscription();
+    subscription(expense_name, expense_amount, expense_date);
   } else if (document.getElementById("expensetype").value == "debt") {
-    debt();
+    debt(expense_name, expense_amount, expense_date);
   }
 
 }
 
-function one_time_expense() {
-  // document.getElementById("out").innerHTML = "Hello, world.";
-  document.getElementById("out1").innerHTML = expense_amount;
-  console.log("One time expense of " + expense_amount);
+function one_time_expense(name, amount, date) {
+  document.getElementById("out1").innerHTML = amount;
+
 }
 
-function subscription() {
+function subscription(name, amount, date) {
   document.getElementById("out").innerHTML = expense_amount;
 }
 
-function debt() {
+function debt(name, amount, date) {
   document.getElementById("out").innerHTML = expense_amount;
 }
