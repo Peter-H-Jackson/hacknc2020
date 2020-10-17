7645dce6ad7bfd862d5be1cd5cb8c3b82accb65c
Peter Jackson <peterhj@live.unc.edu>
2020-10-17 19:05:15 -0400

    Oooooommmyyygodddddd this is soooooo sexyyyyy

--- src/main.js
+++ src/main.js
@@ -1,16 +1,39 @@
-import Expenses from './Expenses.js';
+class Expenses {
+    constructor(name, dateSpent, type, amount) {
+        this.name = name;
+        this.dateSpent = dateSpent;
+        this.type = type;
+        this.amount = amount;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getDate() {
+        return this.dateSpent;
+    }
+
+    getType() {
+        return this.type;
+    }
+
+    getAmount() {
+        return this.amount;
+    }
+
+    printResult() {
+
+    }
+}
 
 function myFunction() {
-
-/*
   var expense = new Expense(
   document.getElementById("expnameinput").value,
   document.getElementById("expamountinput").value,
   document.getElementById("exptypeinput").value,
   document.getElementById("expdateinput").value
   );
-  */
 
-  document.getElementById("output").innerHTML = "fuck you";
-
+  document.getElementById("output").innerHTML = expense.getName();
 }
