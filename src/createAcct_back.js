391d3587dad6b04756c0af87109b2461613f85af
matthewjvardis <mvardis@live.unc.edu>
2020-10-17 19:36:12 -0400

    Yo mama

---
+++ src/createAcct_back.js
@@ -0,0 +1,42 @@
+class User {
+
+  constructor (name, email, password, phone, goals) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.phone = phone;
+    this.goals = goals;
+  }
+
+  getName() {
+    return name;
+  }
+
+  getEmail() {
+    return email;
+  }
+
+  getPassword() {
+    return password;
+  }
+
+  getPhone() {
+    return phone;
+  }
+
+  getGoals() {
+    return goals;
+  }
+}
+
+function createUser() {
+
+  var name, email, password, phone, goals;
+  name = document.getElementById("name").value;
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+  phone = document.getElementById("phone").value;
+  goals = document.getElementById("goals").value;
+
+  var user = new User(name, email, password, phone, goals);
+}
