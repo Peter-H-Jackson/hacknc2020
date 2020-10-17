3c1d0f8fdc25efd5f1fb832fffe9798e7371687c
Cole Vahey <rcvahey01@gmail.com>
2020-10-17 19:48:09 -0400

    adding user data

---
+++ docs/users.js
@@ -0,0 +1,14 @@
+let data = [
+  {
+    "name": "Peter",
+    "email": "pj@gmail.com",
+    "password": "peterpeter",
+    "goals": "Do better with finances"
+  },
+  {
+    "name": "Robbie",
+    "email": "rt@gmail.com",
+    "password": "robbierob",
+    "goals": "Make more on my money"
+  }
+]
