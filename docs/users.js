075e2c142fd6042166d08d607cc1d29e84e368ef
Cole Vahey <rcvahey01@gmail.com>
2020-10-17 20:07:16 -0400

    More users

--- docs/users.js
+++ docs/users.js
@@ -1,14 +1,26 @@
 let data = [
   {
-    "name": "Peter",
+    "name": "peter",
     "email": "pj@gmail.com",
     "password": "peterpeter",
     "goals": "Do better with finances"
   },
   {
-    "name": "Robbie",
+    "name": "robbie",
     "email": "rt@gmail.com",
     "password": "robbierob",
     "goals": "Make more on my money"
+  },
+  {
+    "name": "matthew",
+    "email": "mv@gmail.com",
+    "password": "mattyv",
+    "goals": "Find ways to manage my money"
+  },
+  {
+    "name": "cole",
+    "email": "rcv@gmail.com",
+    "password": "colecole",
+    "goals": "Big bank goals"
   }
 ]
