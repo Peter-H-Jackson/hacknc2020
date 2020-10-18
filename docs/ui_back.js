0ee8850fcee50423cf7ae06de7f86335e30de8e6
Peter Jackson <peterhj@live.unc.edu>
2020-10-18 11:37:16 -0400

    commit

--- docs/ui_back.js
+++ docs/ui_back.js
@@ -1,13 +1,33 @@
+var counter = 0;
+
 function addExp() {
   var title = document.getElementById("titleinput").value;
   var amount = document.getElementById("amountinput").value;
   var date = document.getElementById("dateinput").value;
 
-  document.getElementById("titleinput").innerHTML = "";
-  document.getElementById("amountinput").innerHTML = "";
-  document.getElementById("dateinput").innerHTML = "";
+  document.getElementById("titleinput").value = "";
+  document.getElementById("amountinput").value = "";
+  document.getElementById("dateinput").value = "";
 
-  document.getElementById("title_1").innerHTML = title;
-  document.getElementById("amount_1").innerHTML = amount;
-  document.getElementById("date_1").innerHTML = date;
+  if (counter == 0) {
+    document.getElementById("title_1").innerHTML = title;
+    document.getElementById("amount_1").innerHTML = amount;
+    document.getElementById("date_1").innerHTML = date;
+    counter = counter + 1;
+  } else if (counter == 1) {
+    document.getElementById("title_2").innerHTML = title;
+    document.getElementById("amount_2").innerHTML = amount;
+    document.getElementById("date_2").innerHTML = date;
+    counter = counter + 1;
+  } else if (counter == 2) {
+    document.getElementById("title_3").innerHTML = title;
+    document.getElementById("amount_3").innerHTML = amount;
+    document.getElementById("date_3").innerHTML = date;
+    counter = counter + 1;
+  } else if (counter == 3) {
+    document.getElementById("title_4").innerHTML = title;
+    document.getElementById("amount_4").innerHTML = amount;
+    document.getElementById("date_4").innerHTML = date;
+  }
+
 }
